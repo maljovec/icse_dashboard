348539f83c213754a6706c21f2d49aebd20fdacf
Dan Maljovec <maljovec002@gmail.com>
2018-09-28 14:16:21 -0600

    Genericizing the input

--- server.py
+++ server.py
@@ -41,16 +41,8 @@ configuration['colors'] = {
     'inference': "#4daf4a",
     'default': "#4daf4a"}
 
-configuration['ticks'] = {
-    'O2': [],
-    'HEAT_FLUX': [],
-    'TEMPERATURE': []
-}
-
-configuration['ticks'] = {
-    'O2': [],
-    'HEAT_FLUX': [],
-    'TEMPERATURE': []
+configuration['scales'] = {
+    'Example2': 'shared',
 }
 
 ########################################################################
@@ -105,6 +97,98 @@ def get_sample_points(output_type):
 
     return json.dumps(sample_data)
 
+@app.route("/data")
+def get_test_data():
+    test_data = {"Example": {
+            "simulation": {
+                "0": {
+                    "A": 0.25,
+                    "B": 0.25,
+                    "C": 0.25,
+                    "D": 0.25,
+                },
+                "1": {
+                    "A": 0.0,
+                    "B": 0.1,
+                    "C": 0.0,
+                    "D": 0.1,
+                },
+            },
+            "inference": {
+                "0": {
+                    "A": 0.0,
+                    "B": 0.0,
+                    "C": 0.0,
+                    "D": 0.0,
+                },
+                "1": {
+                    "A": 1.0,
+                    "B": 1.0,
+                    "C": 1.0,
+                    "D": 1.0,
+                },
+                "2": {
+                    "A": 0.5,
+                    "B": 0.0,
+                    "C": 0.5,
+                    "D": 0.0,
+                },
+            },
+            "experiment": {
+                "0": {
+                    "A": [0.5, 1.5],
+                    "B": [0, 0.5],
+                    "C": [0, 0.5],
+                    "D": [0.1, 0.3],
+                }
+            }
+        },
+        "Example2": {
+            "simulation": {
+                "0": {
+                    "x0": 0.0,
+                    "x1": 0.0,
+                    "x2": 0.0,
+                    "x3": 0.0,
+                },
+                "1": {
+                    "x0": 0.5,
+                    "x1": 0.5,
+                    "x2": 0.5,
+                    "x3": 0.5,
+                },
+            },
+            "inference": {
+                "0": {
+                    "x0": 1.0,
+                    "x1": 1.0,
+                    "x2": 1.0,
+                    "x3": 1.0,
+                },
+                "1": {
+                    "x0": 10.0,
+                    "x1": 0.0,
+                    "x2": 10.0,
+                    "x3": 0.0,
+                },
+                "2": {
+                    "x0": 0.0,
+                    "x1": 5.0,
+                    "x2": 0.0,
+                    "x3": 5.0,
+                },
+            },
+            "experiment": {
+                "0": {
+                    "x0": [5.0, 7.5],
+                    "x1": [2.5, 5.0],
+                    "x2": [4.5, 5.5],
+                    "x3": [0.0, 2.0],
+                }
+            }
+        },
+    }
+    return json.dumps(test_data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT_NUMBER, debug=True)
--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,11 +1,5 @@
 d3.queue()
-    .defer(d3.json, "/data/design")
-    .defer(d3.json, "/data/simulation/O2")
-    .defer(d3.json, "/data/simulation/TEMPERATURE")
-    .defer(d3.json, "/data/simulation/HEAT_FLUX")
-    .defer(d3.json, "/data/samples/O2")
-    .defer(d3.json, "/data/samples/TEMPERATURE")
-    .defer(d3.json, "/data/samples/HEAT_FLUX")
+    .defer(d3.json, "/data")
     .defer(d3.json, "/configuration")
     .await(make_graphs);
 
@@ -22,21 +16,23 @@ function map_data_for_pcp(in_data, series_name) {
     return out_data;
 }
 
-var pcp_inputs;
-var pcp_O2;
-var pcp_temperature;
-var pcp_heat_flux;
+var pcps = {};
 
 function format_dimensions(data) {
     min_value = null;
     max_value = null;
+
     for (var i in data) {
-        for (var j in data[i]) {
-            if (min_value == null || data[i][j] < min_value) {
-                min_value = data[i][j];
+        for (const [key, value] of Object.entries(data[i])) {
+            if (key == 'series' || key == 'key') {
+                continue;
             }
-            if (max_value == null || data[i][j] > max_value) {
-                max_value = data[i][j];
+
+            if (min_value == null || value < min_value) {
+                min_value = value;
+            }
+            if (max_value == null || value > max_value) {
+                max_value = value;
             }
         }
     }
@@ -65,13 +61,12 @@ function format_dimensions(data) {
 
 function update_selection() {
     var current_selection = null;
-    var pcps = [pcp_inputs, pcp_O2, pcp_temperature, pcp_heat_flux];
-    for (var i = 0; i < pcps.length; i++) {
-        var items = pcps[i].brushed();
+    for (const [title, pcp] of Object.entries(pcps)) {
+        var items = pcp.brushed();
         // if something appears as selected and there is at least one
         // dimension that has been brushed for this PCP, then add its
         // selection to the list of highlighted data.
-        if (items && Object.keys(pcps[i].brushExtents()).length) {
+        if (items && Object.keys(pcp.brushExtents()).length) {
             var selected = new Set(items.map(function (d) { return d.key + '_' + d.series; }));
             if (current_selection != null) {
                 current_selection = new Set(intersect(current_selection, selected));
@@ -82,28 +77,27 @@ function update_selection() {
         }
     }
     if (current_selection != null && current_selection.size) {
-        for (var i = 0; i < pcps.length; i++) {
-            pcps[i].clear("highlight");
-            pcps[i].highlight(pcps[i].data().filter((item, i) => current_selection.has(item.key + '_' + item.series)));
+        for (const [title, pcp] of Object.entries(pcps)) {
+            pcp.clear("highlight");
+            pcp.highlight(pcp.data().filter((item, i) => current_selection.has(item.key + '_' + item.series)));
         }
     }
     else {
-        for (var i = 0; i < pcps.length; i++) {
-            pcps[i].clear("highlight");
-            pcps[i].canvas.foreground.classList.remove('faded');
+        for (const [title, pcp] of Object.entries(pcps)) {
+            pcp.clear("highlight");
+            pcp.canvas.foreground.classList.remove('faded');
         }
     }
 }
 
 function preview_selection(label) {
     var current_selection = null;
-    var pcps = [pcp_inputs, pcp_O2, pcp_temperature, pcp_heat_flux];
-    for (var i = 0; i < pcps.length; i++) {
-        var items = pcps[i].brushed();
+    for (const [title, pcp] of Object.entries(pcps)) {
+        var items = pcp.brushed();
         // if something appears as selected and there is at least one
         // dimension that has been brushed for this PCP, then add its
         // selection to the list of highlighted data.
-        if (items && Object.keys(pcps[i].brushExtents()).length) {
+        if (items && Object.keys(pcp.brushExtents()).length) {
             var selected = new Set(items.map(function (d) { return d.key + '_' + d.series; }));
             if (current_selection != null) {
                 current_selection = new Set(intersect(current_selection, selected));
@@ -114,15 +108,15 @@ function preview_selection(label) {
         }
     }
     if (current_selection != null && current_selection.size) {
-        for (var i = 0; i < pcps.length; i++) {
-            pcps[i].clear("highlight");
-            pcps[i].highlight(pcps[i].data().filter((item, i) => current_selection.has(item.key + '_' + item.series) && item.series == label));
+        for (const [title, pcp] of Object.entries(pcps)) {
+            pcp.clear("highlight");
+            pcp.highlight(pcp.data().filter((item, i) => current_selection.has(item.key + '_' + item.series) && item.series == label));
         }
     }
     else {
-        for (var i = 0; i < pcps.length; i++) {
-            pcps[i].clear("highlight");
-            pcps[i].highlight(pcps[i].data().filter((item, i) => item.series == label));
+        for (const [title, pcp] of Object.entries(pcps)) {
+            pcp.clear("highlight");
+            pcp.highlight(pcp.data().filter((item, i) => item.series == label));
         }
     }
 }
@@ -153,7 +147,7 @@ function create_parcoord(box, title, data, dimensions, colormap) {
             .shadows()
             .reorderable()
             .brushMode("1D-axes")
-            .rotateLabels(true)
+            .rotateLabels(false)
             .on("brush", update_selection)
             .on("render", update_selection);
     }
@@ -203,7 +197,7 @@ function create_parcoord(box, title, data, dimensions, colormap) {
     return parcoords;
 }
 
-function make_graphs(error, input_data, simO2, simT, simHF, samplesO2, samplesT, samplesHF, config) {
+function make_graphs(error, input_data, config) {
     if (error != null) {
         console.log(error);
         return;
@@ -270,67 +264,23 @@ function make_graphs(error, input_data, simO2, simT, simHF, samplesO2, samplesT,
         .attr('class', 'spacer')
         .attr('style', 'height: 100px;');
 
-    var experimental_data = {
-        "Base_Case": {
-            "AbsCD":[0.5,1.5],
-            "CO2_PC1":[0,0.5],
-            "CO2_PC2":[0,0.5],
-            "Csmag":[0.1,0.3],
-            "O2_PC1":[0.0,0.5],
-            "O2_PC2":[0.0,0.5],
-            "ThardB":[1825,1875],
-            "Tslag":[1500,1520],
-            "devolTmu":[820,832],
-            "key":"Case_1",
-            "ln(Avisc)":[-54.654074025,-50],
-            "log(Kdeposit)":[0,0.5],
-            "log(enam)":[-4,-2],
-            "series":"experiment",
-            "tSootBlow":[2,8]
+    for (const [title, data_object] of Object.entries(input_data)) {
+        var dimensions;
+        var data = [];
+        for (const [series, values] of Object.entries(data_object)) {
+            data = data.concat(map_data_for_pcp(values, series));
         }
-    };
-    var data = map_data_for_pcp(input_data, "inference").concat(map_data_for_pcp(input_data, "simulation"));
-    data = data.concat(map_data_for_pcp(experimental_data, "experiment"));
-    var dimensions = null; //format_dimensions(data);
-    var title = 'Inputs';
-    var box = d3.select('body').append('div').attr('class', 'box');
-    pcp_inputs = create_parcoord(box, "", data, dimensions, colormap);
-    // box.append('h1').html(title);
-    // box.append('div')
-    //     .attr('id', 'pcp_' + title)
-    //     .attr('class', 'parcoords')
-    //     .style("width", "100%")
-    //     .style("height", "150px")
-    //     .style("z-index", 1);
 
-    // pcp_inputs = d3.parcoords()("#pcp_" + title)
-    //     .data(data)
-    //     .hideAxis(["key", "series"])
-    //     .color(colormap)
-    //     .render()
-    //     .shadows()
-    //     .reorderable()
-    //     // .smoothness(1)
-    //     .axisDots(size = 1)
-    //     // .bundleDimension("ThardB")
-    //     .rotateLabels(true)
-    //     .brushMode("1D-axes")
-    //     .on("brush", update_selection)
-    //     .on("render", update_selection);
-
-    var output_box = d3.select('body').append('div').attr('class', 'box');
-    output_box.append('h1').html("Outputs");
-    data = map_data_for_pcp(samplesO2, "inference").concat(map_data_for_pcp(simO2, "simulation"));
-    dimensions = format_dimensions(data);
-    pcp_O2 = create_parcoord(output_box, 'O2', data, dimensions, colormap);
-
-    data = map_data_for_pcp(samplesT, "inference").concat(map_data_for_pcp(simT, "simulation"));
-    dimensions = format_dimensions(data);
-    pcp_temperature = create_parcoord(output_box, 'Temperature', data, dimensions, colormap);
-
-    data = map_data_for_pcp(samplesHF, "inference").concat(map_data_for_pcp(simHF, "simulation"));
-    dimensions = format_dimensions(data);
-    pcp_heat_flux = create_parcoord(output_box, 'Heat_Flux', data, dimensions, colormap);
+        if (title in config['scales'] && config['scales'][title] == 'shared') {
+            dimensions = format_dimensions(data);
+        }
+        else {
+            dimensions = null;
+        }
+        var box = d3.select('body').append('div').attr('class', 'box');
+        console.log(title, data);
+        pcps[title] = create_parcoord(box, title, data, dimensions, colormap);
+    }
 
     d3.selectAll(".dimension")
         .selectAll(".tick text")
