2ae9c8407fb6daceb06fb830cf2e63dcba16aafb
Dan Maljovec <maljovec002@gmail.com>
2018-10-25 22:49:49 -0500

    Updating colors on simple example to better test new UI elements.

--- example_simple.py
+++ example_simple.py
@@ -1,11 +1,15 @@
 from quetzal import Dashboard
 
+# Black and yellow specifically chosen for the dimension spans to
+# ensure dark on dark button and light with highlight on dark button
+# look okay
 configuration = {
     "colors": {
                 "Series 1": "#e41a1c",
                 "Series 2": "#377eb8",
-                "Series 3": "#4daf4a",
-                "default": "#000000",
+                "Series 3": "#000000",
+                "Series 5": "#ffff33",
+                "default": "#4daf4a",
             },
     "scales":  {"Dimension Set 2": "shared"},
     "labels": {"Dimension Set 1": "none"},
@@ -33,6 +37,14 @@ data = {
         "Series 4": {
             "Case 0": {"A": 0.25, "B": 0.75, "C": 0.25, "D": 0.75},
         },
+        "Series 5": {
+            "Case 0": {
+                "A": [0.0, 0.3],
+                "B": [0.25, 0.75],
+                "C": [0.6, 0.8],
+                "D": [0.4, 0.5, 0.6],
+            }
+        },
     },
     "Dimension Set 2": {
         "Series 1": {
@@ -50,6 +62,14 @@ data = {
                 "x1": [2.5, 5.0],
                 "x2": [4.5, 5.5],
                 "x3": [0.0, 2.0],
+            }
+        },
+        "Series 5": {
+            "Case 0": {
+                "x0": [2.5, 4.5],
+                "x1": [6.0, 7.0],
+                "x2": [1.0, 3.0],
+                "x3": [0.3, 4.5],
             }
         },
     },
